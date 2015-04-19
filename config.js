b46b605d14100de5ce1fdee3a0b2689c5b485554
hongmomanu <hongmomanu@gmail.com>
2015-04-19 17:30:22 +0800

    jack commit at home

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 var Globle_Variable={
-    serverurl:"http://192.168.168.101:3000/",
+    //serverurl:"http://192.168.168.101:3000/",
+    serverurl:"http://192.168.11.2:3000/",
     user:null,
     custompush:null,
-    askmin:30
+    askmin:30,
+    isactived:true
 }
