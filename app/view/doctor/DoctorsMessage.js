fe4f92c38cd088bad65cbf4a4530e3f9aea221d5
jack <hongmomanu@163.com>
2015-04-21 13:54:08 +0800

    jack commit

--- app/view/doctor/DoctorsMessage.js
+++ app/view/doctor/DoctorsMessage.js
@@ -52,17 +52,162 @@ Ext.define('PatientApp.view.doctor.DoctorsMessage', {
 
                         {
                             xtype:'button',
-                            iconCls:'action',
+                            iconCls:'voice',
                             listeners: {
                                 element: 'element',
-                                taphold : function() {
-                                    Ext.Msg.alert('test', '', Ext.emptyFn);
+                                touchstart : function() {
+
+                                    var me=this;
+
+                                    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,gotFS , function(){
+
+                                    });
+                                    function gotFS(fileSystem) {
+                                        fileSystem.root.getFile("blank.wav", {create: true, exclusive: false}, gotFileEntry,  function(){
+
+                                        });
+
+                                        function gotFileEntry(fileEntry) {
+
+                                            var src=fileEntry.toInternalURL();
+                                            //Ext.Msg.alert("filepath",src);
+
+                                            var mediaRec = new Media(src,
+                                                // success callback
+                                                function() {
+
+                                                    //Ext.Msg.alert("success","recordAudio():Audio Success");
+
+
+
+
+
+                                                },
+
+                                                // error callback
+                                                function(err) {
+                                                    //console.log("recordAudio():Audio Error: "+ err.code);
+                                                    Ext.Msg.alert("success","recordAudio():Audio Success"+ err.code);
+                                                });
+
+                                            // Record audio
+                                            mediaRec.startRecord();
+
+                                            setTimeout(function() {
+                                                Ext.Msg.alert('end', src, Ext.emptyFn);
+                                                mediaRec.stopRecord();
+
+
+                                                var win = function (r) {
+                                                    Ext.Msg.alert('seccess',r.response);
+                                                }
+
+                                                var fail = function (error) {
+                                                    Ext.Msg.alert('error',"An error has occurred: Code = " + error.code);
+
+                                                }
+
+                                                var options = new FileUploadOptions();
+                                                options.fileKey = "file";
+                                                options.fileName = src.substr(src.lastIndexOf('/') + 1);
+                                                options.mimeType = "audio/wav";
+
+
+                                                var ft = new FileTransfer();
+                                                //Ext.Msg.alert('seccess',Globle_Variable.serverurl+'common/uploadfile');
+                                                ft.upload(src, encodeURI(Globle_Variable.serverurl+'common/uploadfile'), win, fail, options);
+
+
+
+
+                                            }, 3000);
+
+
+
+                                        }
+                                    }
+
+                                    /*window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dir) {
+                                        //Ext.Msg.alert("got the file", dir)
+                                        dir.getFile("myrecording.wav", {create:true}, function(file) {
+
+                                            var src=file.toInternalURL();
+                                            //var src=file.toURL();
+                                            me.mediaRec = new Media(src,
+                                                // success callback
+                                                function() {
+
+                                                    //Ext.Msg.alert("success","recordAudio():Audio Success");
+
+
+
+
+
+                                                },
+
+                                                // error callback
+                                                function(err) {
+                                                    //console.log("recordAudio():Audio Error: "+ err.code);
+                                                    Ext.Msg.alert("success","recordAudio():Audio Success"+ err.code);
+                                                });
+
+                                            // Record audio
+                                            me.mediaRec.startRecord();
+
+                                            setTimeout(function() {
+                                                Ext.Msg.alert('end', src, Ext.emptyFn);
+                                                me.mediaRec.stopRecord();
+
+
+                                                var win = function (r) {
+                                                    Ext.Msg.alert('seccess',r.response);
+                                                }
+
+                                                var fail = function (error) {
+                                                    Ext.Msg.alert('error',"An error has occurred: Code = " + error.code);
+
+                                                }
+
+                                                var options = new FileUploadOptions();
+                                                options.fileKey = "file";
+                                                options.fileName = src.substr(src.lastIndexOf('/') + 1);
+                                                options.mimeType = "audio/wav";
+
+
+                                                var ft = new FileTransfer();
+                                                //Ext.Msg.alert('seccess',Globle_Variable.serverurl+'common/uploadfile');
+                                                ft.upload(src, encodeURI(Globle_Variable.serverurl+'common/uploadfile'), win, fail, options);
+
+
+
+
+                                            }, 10000);
+
+
+
+                                            //Ext.Msg.alert("got the file", file.toURL())
+                                            //console.log("got the file", file);
+
+
+
+
+                                        });
+                                    });*/
+
+
+
+
+                                },
+                                touchend : function() {
+                                    //this.mediaRec.stopRecord();
+                                    //Ext.Msg.alert('end', 'finished', Ext.emptyFn);
                                 }
+
                             }
                         },
                         {
                             xtype:'button',
-                            iconCls:'time',
+                            iconCls:'picture',
                             itemId:'choosepic'
                         },
                         {
